8e36118d5e2d147bf7dced61bdfa7ef220bedd9a
loftyGiraffe <94640313+loftyGiraffe@users.noreply.github.com>
2021-11-28 22:43:32 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -80,7 +80,7 @@ export default function Home() {
                         letterSpacing: "6px",
                     }}
                 >
-                       Whitelist Raffle #1842
+                       Whitelist #1842
                 </div>
             </div>
             <div
@@ -144,7 +144,7 @@ export default function Home() {
                         borderRadius: "6px",
                     }}
                     min="1"
-                    max="5"
+                    max="10"
                     type="number"
                 />
                 <div style={{ color: "white", fontSize: "30px", fontWeight: '500', }}>Cryptobull(s)</div>
